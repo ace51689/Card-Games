a6a22b012461bcfe6cabe750a721b01c59564d3d
Michael Vitollo <ace51689@gmail.com>
2020-06-11 21:17:23 -0400

    Ver 1.2 FTD Card table added and functioning, Guess buttons now are inactive when not meant to be used.

--- main.js
+++ main.js
@@ -12,19 +12,19 @@ let ftdCard = document.querySelector('#ftdCard')
 let message = document.querySelector('#message')
 let firstGuessDisplay = document.querySelector('#firstGuessDisplay')
 let finalGuessDisplay = document.querySelector('#finalGuessDisplay')
-// let twos = document.querySelector('#twos')
-// let threes = document.querySelector('#threes')
-// let fours = document.querySelector('#fours')
-// let fives = document.querySelector('#fives')
-// let sixes = document.querySelector('#sixes')
-// let sevens = document.querySelector('#sevens')
-// let eights = document.querySelector('#eights')
-// let nines = document.querySelector('#nines')
-// let tens = document.querySelector('#tens')
-// let jacks = document.querySelector('#jacks')
-// let queens = document.querySelector('#queens')
-// let kings = document.querySelector('#kings')
-// let aces = document.querySelector('#aces')
+let twos = document.querySelector('#twos')
+let threes = document.querySelector('#threes')
+let fours = document.querySelector('#fours')
+let fives = document.querySelector('#fives')
+let sixes = document.querySelector('#sixes')
+let sevens = document.querySelector('#sevens')
+let eights = document.querySelector('#eights')
+let nines = document.querySelector('#nines')
+let tens = document.querySelector('#tens')
+let jacks = document.querySelector('#jacks')
+let queens = document.querySelector('#queens')
+let kings = document.querySelector('#kings')
+let aces = document.querySelector('#aces')
 
 //Kings---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 let deck = ['2 &hearts;', '2 &diams;', '2 &spades;', '2 &clubs;', '3 &hearts;', '3 &diams;', '3 &spades;', '3 &clubs;',
@@ -92,6 +92,33 @@ reset.addEventListener('click', function () {
 })
 
 //Fuck The Dealer-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+let two = 4
+let three = 4
+let four = 4
+let five = 4
+let six = 4
+let seven = 4
+let eight = 4
+let nine = 4
+let ten = 4
+let jack = 4
+let queen = 4
+let king = 4
+let ace = 4
+twos.innerText = (two)
+threes.innerText = (three)
+fours.innerText = (four)
+fives.innerText = (five)
+sixes.innerText = (six)
+sevens.innerText = (seven)
+eights.innerText = (eight)
+nines.innerText = (nine)
+tens.innerText = (ten)
+jacks.innerText = (jack)
+queens.innerText = (queen)
+kings.innerText = (king)
+aces.innerText = (ace)
+
 let currFtdCard = []
 let ftdDeck = ['2 &hearts;', '2 &diams;', '2 &spades;', '2 &clubs;', '3 &hearts;', '3 &diams;', '3 &spades;', '3 &clubs;',
     '4 &hearts;', '4 &diams;', '4 &spades;', '4 &clubs;', '5 &hearts;', '5 &diams;', '5 &spades;', '5 &clubs;', '6 &hearts;', '6 &diams;',
@@ -99,7 +126,7 @@ let ftdDeck = ['2 &hearts;', '2 &diams;', '2 &spades;', '2 &clubs;', '3 &hearts;
     '9 &hearts;', '9 &diams;', '9 &spades;', '9 &clubs;', '10 &hearts;', '10 &diams;', '10 &spades;', '10 &clubs;', 'J &hearts;',
     'J &diams;', 'J &spades;', 'J &clubs;', 'Q &hearts;', 'Q &diams;', 'Q &spades;', 'Q &clubs;', 'K &hearts;', 'K &diams;', 'K &spades;',
     'K &clubs;', 'A &hearts;', 'A &diams;', 'A &spades;', 'A &clubs;',]
-
+finGuess.disabled = true
 oneGuess.addEventListener('click', function () {
     currFtdCard = []
     finalGuess.value = (' ')
@@ -117,7 +144,7 @@ oneGuess.addEventListener('click', function () {
         let guessCheck = ftdDeck[outer]
         if (guessCheck.includes(firstGuessString)) {
             guessArray.push(outer)
-        } 
+        }
         // else if (guessArray = []) {
         //     // alert("There are no more cards of that value")
         //     // { break; }
@@ -125,6 +152,58 @@ oneGuess.addEventListener('click', function () {
     }
     if (guessArray.includes(cardIndex)) {
         message.innerText = ('You Got It!')
+        if (currFtdCard.includes('2')) {
+            two = two - 1
+            twos.innerText = two
+        }
+        else if (currFtdCard.includes('3')) {
+            three = three - 1
+            threes.innerText = three
+        }
+        else if (currFtdCard.includes('4')) {
+            four = four - 1
+            fours.innerText = four
+        }
+        else if (currFtdCard.includes('5')) {
+            five = five - 1
+            fives.innerText = five
+        }
+        else if (currFtdCard.includes('6')) {
+            six = six - 1
+            sixes.innerText = six
+        }
+        else if (currFtdCard.includes('7')) {
+            seven = seven - 1
+            sevens.innerText = seven
+        }
+        else if (currFtdCard.includes('8')) {
+            eight = eight - 1
+            eights.innerText = eight
+        }
+        else if (currFtdCard.includes('9')) {
+            nine = nine - 1
+            nines.innerText = nine
+        }
+        else if (currFtdCard.includes('10')) {
+            ten = ten - 1
+            tens.innerText = ten
+        }
+        else if (currFtdCard.includes('J')) {
+            jack = jack - 1
+            jacks.innerText = jack
+        }
+        else if (currFtdCard.includes('Q')) {
+            queen = queen - 1
+            queens.innerText = queen
+        }
+        else if (currFtdCard.includes('K')) {
+            king = king - 1
+            kings.innerText = king
+        }
+        else if (currFtdCard.includes('A')) {
+            ace = ace - 1
+            aces.innerText = ace
+        }
         let popCard = currFtdCard.pop()
         ftdCard.innerHTML = popCard
         ftdDeck.splice(cardIndex, 1)
@@ -132,25 +211,84 @@ oneGuess.addEventListener('click', function () {
     } else if (cardIndex < guessArray[0]) {
         message.innerText = ('Lower')
         ftdDeck.splice(cardIndex, 1)
+        oneGuess.disabled = true
+        finGuess.disabled = false
+
     } else if (cardIndex > guessArray[0]) {
         message.innerText = ('Higher')
         ftdDeck.splice(cardIndex, 1)
+        oneGuess.disabled = true
+        finGuess.disabled = false
     }
     console.log(ftdDeck.length + ' is how many cards are left')
+    console.log(currFtdCard)
 })
 
 finGuess.addEventListener('click', function () {
-    // firstGuess.value = (' ')
+    oneGuess.disabled = false
+    finGuess.disabled = true
     let finalGuessValue = finalGuess.value.trim()
     let finalGuessString = finalGuessValue.toString()
     finalGuessDisplay.innerText = ('Final Guess: ' + finalGuessString)
     if (currFtdCard.toString().includes(finalGuessString)) {
+        if (currFtdCard.toString().includes('2')) {
+            two = two - 1
+            twos.innerText = two
+        }
+        else if (currFtdCard.toString().includes('3')) {
+            three = three - 1
+            threes.innerText = three
+        }
+        else if (currFtdCard.toString().includes('4')) {
+            four = four - 1
+            fours.innerText = four
+        }
+        else if (currFtdCard.toString().includes('5')) {
+            five = five - 1
+            fives.innerText = five
+        }
+        else if (currFtdCard.toString().includes('6')) {
+            six = six - 1
+            sixes.innerText = six
+        }
+        else if (currFtdCard.toString().includes('7')) {
+            seven = seven - 1
+            sevens.innerText = seven
+        }
+        else if (currFtdCard.toString().includes('8')) {
+            eight = eight - 1
+            eights.innerText = eight
+        }
+        else if (currFtdCard.toString().includes('9')) {
+            nine = nine - 1
+            nines.innerText = nine
+        }
+        else if (currFtdCard.toString().includes('10')) {
+            ten = ten - 1
+            tens.innerText = ten
+        }
+        else if (currFtdCard.toString().includes('J')) {
+            jack = jack - 1
+            jacks.innerText = jack
+        }
+        else if (currFtdCard.toString().includes('Q')) {
+            queen = queen - 1
+            queens.innerText = queen
+        }
+        else if (currFtdCard.toString().includes('K')) {
+            king = king - 1
+            kings.innerText = king
+        }
+        else if (currFtdCard.toString().includes('A')) {
+            ace = ace - 1
+            aces.innerText = ace
+        }
         let popCard = currFtdCard.pop(0)
         ftdCard.innerHTML = popCard
         message.innerText = ('You Got It!')
     } else {
         message.innerText = ('Nope, Drink Bitch.')
         let popCard = currFtdCard.pop(0)
-        ftdCard.innerHTML = popCard
+        ftdCard.innerText = popCard
     }
 })
