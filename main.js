a3fc05e9ba2aa7d5155144317f316a2733a64ffe
Michael Vitollo <ace51689@gmail.com>
2020-06-12 22:30:01 -0400

    Ver 1.2.1 Minor big fixes, FTD counters work for all outcomes, added toUpperCase to the guess input strings.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-//Universal Declareations---------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Kings Declareations---------------------------------------------------------------------------------------------------------------------------------------------------------------
 let draw = document.querySelector('#drawCard')
 let currCard = document.querySelector('#currCard')
 let action = document.querySelector('#action')
 let cardsRemain = document.querySelector('#cardsRemain')
-let reset = document.querySelector('#reset')
+let kingsReset = document.querySelector('#kingsReset')
 let firstGuess = document.querySelector('#firstGuess')  //Input
 let finalGuess = document.querySelector('#finalGuess')  //Input
 let oneGuess = document.querySelector('#oneGuess')  //Button
@@ -41,14 +41,10 @@ draw.addEventListener('click', function () {
     let totIndex = (deck.length)
     let pullCard = Math.floor(Math.random() * totIndex)
     let card = deck.splice(pullCard, 1)
-    console.log(card)
-    console.log(totIndex)
-
     let newCard = '<li>' + card + '</li>'
     currCard.innerHTML = newCard
-
     cardsRemain.innerText = ((deck.length) + ' cards remaining')
-
+    
     let cardString = card.toString()
     if (cardString.includes('2')) {
         action.innerText = ('IS FOR YOU!')
@@ -79,7 +75,7 @@ draw.addEventListener('click', function () {
     }
 })
 
-reset.addEventListener('click', function () {
+kingsReset.addEventListener('click', function () {
     deck = ['2 &hearts;', '2 &diams;', '2 &spades;', '2 &clubs;', '3 &hearts;', '3 &diams;', '3 &spades;', '3 &clubs;',
         '4 &hearts;', '4 &diams;', '4 &spades;', '4 &clubs;', '5 &hearts;', '5 &diams;', '5 &spades;', '5 &clubs;', '6 &hearts;', '6 &diams;',
         '6 &spades;', '6 &clubs;', '7 &hearts;', '7 &diams;', '7 &spades;', '7 &clubs;', '8 &hearts;', '8 &diams;', '8 &spades;', '8 &clubs;',
@@ -130,13 +126,16 @@ finGuess.disabled = true
 oneGuess.addEventListener('click', function () {
     currFtdCard = []
     finalGuess.value = (' ')
+    ftdCard.innerHTML = (' ')
     let totIndex = ftdDeck.length
     let randomIndex = Math.floor(Math.random() * totIndex)
     let card = ftdDeck[randomIndex]
     let cardIndex = ftdDeck.indexOf(card)
     currFtdCard.push(card)
+    console.log(currFtdCard)
     let firstGuessValue = firstGuess.value.trim()
-    let firstGuessString = firstGuessValue.toString()
+    let firstGuessString = firstGuessValue.toString().toUpperCase()
+    console.log(firstGuessString)
     firstGuessDisplay.innerText = ('First Guess: ' + firstGuessString)
     finalGuessDisplay.innerText = ('Final Guess:')
     let guessArray = []
@@ -152,85 +151,6 @@ oneGuess.addEventListener('click', function () {
     }
     if (guessArray.includes(cardIndex)) {
         message.innerText = ('You Got It!')
-        if (currFtdCard.includes('2')) {
-            two = two - 1
-            twos.innerText = two
-        }
-        else if (currFtdCard.includes('3')) {
-            three = three - 1
-            threes.innerText = three
-        }
-        else if (currFtdCard.includes('4')) {
-            four = four - 1
-            fours.innerText = four
-        }
-        else if (currFtdCard.includes('5')) {
-            five = five - 1
-            fives.innerText = five
-        }
-        else if (currFtdCard.includes('6')) {
-            six = six - 1
-            sixes.innerText = six
-        }
-        else if (currFtdCard.includes('7')) {
-            seven = seven - 1
-            sevens.innerText = seven
-        }
-        else if (currFtdCard.includes('8')) {
-            eight = eight - 1
-            eights.innerText = eight
-        }
-        else if (currFtdCard.includes('9')) {
-            nine = nine - 1
-            nines.innerText = nine
-        }
-        else if (currFtdCard.includes('10')) {
-            ten = ten - 1
-            tens.innerText = ten
-        }
-        else if (currFtdCard.includes('J')) {
-            jack = jack - 1
-            jacks.innerText = jack
-        }
-        else if (currFtdCard.includes('Q')) {
-            queen = queen - 1
-            queens.innerText = queen
-        }
-        else if (currFtdCard.includes('K')) {
-            king = king - 1
-            kings.innerText = king
-        }
-        else if (currFtdCard.includes('A')) {
-            ace = ace - 1
-            aces.innerText = ace
-        }
-        let popCard = currFtdCard.pop()
-        ftdCard.innerHTML = popCard
-        ftdDeck.splice(cardIndex, 1)
-        guessArray.splice(cardIndex)
-    } else if (cardIndex < guessArray[0]) {
-        message.innerText = ('Lower')
-        ftdDeck.splice(cardIndex, 1)
-        oneGuess.disabled = true
-        finGuess.disabled = false
-
-    } else if (cardIndex > guessArray[0]) {
-        message.innerText = ('Higher')
-        ftdDeck.splice(cardIndex, 1)
-        oneGuess.disabled = true
-        finGuess.disabled = false
-    }
-    console.log(ftdDeck.length + ' is how many cards are left')
-    console.log(currFtdCard)
-})
-
-finGuess.addEventListener('click', function () {
-    oneGuess.disabled = false
-    finGuess.disabled = true
-    let finalGuessValue = finalGuess.value.trim()
-    let finalGuessString = finalGuessValue.toString()
-    finalGuessDisplay.innerText = ('Final Guess: ' + finalGuessString)
-    if (currFtdCard.toString().includes(finalGuessString)) {
         if (currFtdCard.toString().includes('2')) {
             two = two - 1
             twos.innerText = two
@@ -283,12 +203,91 @@ finGuess.addEventListener('click', function () {
             ace = ace - 1
             aces.innerText = ace
         }
+        let popCard = currFtdCard.pop()
+        ftdCard.innerHTML = popCard
+        ftdDeck.splice(cardIndex, 1)
+        guessArray.splice(cardIndex)
+    } else if (cardIndex < guessArray[0]) {
+        message.innerText = ('Lower')
+        ftdDeck.splice(cardIndex, 1)
+        oneGuess.disabled = true
+        finGuess.disabled = false
+
+    } else if (cardIndex > guessArray[0]) {
+        message.innerText = ('Higher')
+        ftdDeck.splice(cardIndex, 1)
+        oneGuess.disabled = true
+        finGuess.disabled = false
+    }
+    console.log(ftdDeck.length + ' is how many cards are left')
+    console.log(currFtdCard)
+})
+
+finGuess.addEventListener('click', function () {
+    oneGuess.disabled = false
+    finGuess.disabled = true
+    let finalGuessValue = finalGuess.value.trim()
+    let finalGuessString = finalGuessValue.toString().toUpperCase()
+    finalGuessDisplay.innerText = ('Final Guess: ' + finalGuessString)
+    if (currFtdCard.toString().includes('2')) {
+        two = two - 1
+        twos.innerText = two
+    }
+    else if (currFtdCard.toString().includes('3')) {
+        three = three - 1
+        threes.innerText = three
+    }
+    else if (currFtdCard.toString().includes('4')) {
+        four = four - 1
+        fours.innerText = four
+    }
+    else if (currFtdCard.toString().includes('5')) {
+        five = five - 1
+        fives.innerText = five
+    }
+    else if (currFtdCard.toString().includes('6')) {
+        six = six - 1
+        sixes.innerText = six
+    }
+    else if (currFtdCard.toString().includes('7')) {
+        seven = seven - 1
+        sevens.innerText = seven
+    }
+    else if (currFtdCard.toString().includes('8')) {
+        eight = eight - 1
+        eights.innerText = eight
+    }
+    else if (currFtdCard.toString().includes('9')) {
+        nine = nine - 1
+        nines.innerText = nine
+    }
+    else if (currFtdCard.toString().includes('10')) {
+        ten = ten - 1
+        tens.innerText = ten
+    }
+    else if (currFtdCard.toString().includes('J')) {
+        jack = jack - 1
+        jacks.innerText = jack
+    }
+    else if (currFtdCard.toString().includes('Q')) {
+        queen = queen - 1
+        queens.innerText = queen
+    }
+    else if (currFtdCard.toString().includes('K')) {
+        king = king - 1
+        kings.innerText = king
+    }
+    else if (currFtdCard.toString().includes('A')) {
+        ace = ace - 1
+        aces.innerText = ace
+    }
+    if (currFtdCard.toString().includes(finalGuessString)) {
         let popCard = currFtdCard.pop(0)
         ftdCard.innerHTML = popCard
         message.innerText = ('You Got It!')
     } else {
         message.innerText = ('Nope, Drink Bitch.')
         let popCard = currFtdCard.pop(0)
-        ftdCard.innerText = popCard
+        ftdCard.innerHTML = popCard
     }
 })
